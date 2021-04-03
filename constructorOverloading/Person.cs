af61ac2326b5cc73d36918f7916b22519d791e0d
unknown <jobayerdpi11@gmail.com>
2021-04-03 08:30:38 +0600

    default constructor and constructor overloading

---
+++ constructorOverloading/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructorOverloading
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string middleName, string lastName)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+        }
+
+        public Person(string middleName, string lastName)
+        {
+            MiddleName = middleName;
+            LastName = lastName;
+        }
+
+        public Person()
+        {
+            //this is default constructor
+        }
+
+        public string getFullName()
+        {
+            string myName=FirstName+" "+MiddleName+" "+LastName;
+            return myName;
+        } 
+    }
+}
---
+++ constructorOverloading/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructorOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person3 = new Person(); //default constructor
+            Person person1 = new Person("Md.","Hamidur","Rahman");
+            Person person2 = new Person("Sohel","Khan");
+            string fullName = person1.getFullName();
+            string fullName2 = person2.getFullName();
+            Console.WriteLine(fullName);
+            Console.WriteLine(fullName2);
+            Console.ReadKey();
+        }
+    }
+}
